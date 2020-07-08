17f234d8fa3542918d8755ddd88760f3a706bcea
ngaisteve1 <findingsteve@gmail.com>
2020-07-09 07:54:35 +0800

    initial

---
+++ BlazorWebAssemblyMaps/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
+
+namespace BlazorWebAssemblyMaps
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjgxMTU2QDMxMzgyZTMyMmUzMGFzQmRXbWp1NWZKSGVHTUxYQ3d1OVpZR2lZNzQ5TklMeHVlUjNQSkVuRFE9");
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddSyncfusionBlazor();
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
